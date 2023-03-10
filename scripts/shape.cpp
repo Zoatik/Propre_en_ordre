31356258b97bd2538e8bfb6b608698ef190236ad
Zoatik <axelhal@hotmail.com>
2023-03-10 23:42:03 +0100

    0/1 -> false/true
    juste parce que c'est plus joli (et aussi parce que ce sont des bools donc autant utiliser les false/true qui clarifient le code hehe )

--- scripts/shape.cpp
+++ scripts/shape.cpp
@@ -11,10 +11,11 @@ Shape::~Shape()
 
 }
 
+/**TESTS DE COLLISIONS**/
 bool Shape::collision(circle c1, square s1)
 {  
-    s_2d vector(abs(c1.m_center.m_x-s1.m_center.m_x)-s1.m_size/2, abs(c1.m_center.m_y-s1.m_center.m_y)-s1.m_size/2);
-    double L(norm(vector));
+    s_2d vect_2d(abs(c1.m_center.m_x-s1.m_center.m_x)-s1.m_size/2, abs(c1.m_center.m_y-s1.m_center.m_y)-s1.m_size/2);
+    double L(norm(vect_2d));
     if( abs(c1.m_center.m_x-s1.m_center.m_x)<s1.m_size/2+c1.m_radius+epsil_zero and
         abs(c1.m_center.m_y-s1.m_center.m_y)<s1.m_size/2+c1.m_radius+epsil_zero)
     {
@@ -22,21 +23,21 @@ bool Shape::collision(circle c1, square s1)
             abs(c1.m_center.m_y-s1.m_center.m_y)>s1.m_size/2) and
             L > c1.m_radius+epsil_zero)
         {
-            return 0;
+            return false;
         }else{
-            return 1;
+            return true;
         }
     }
-    return 0;
+    return false;
 }
 
 bool Shape::collision(circle c1, circle c2)
 {
     if(distance(c1.m_center, c2.m_center)< (c1.m_radius + c2.m_radius) + epsil_zero)
     {
-        return 1;
+        return true;
     }
-    return 0;
+    return false;
 }
 
 bool Shape::collision(square s1, square s2)
@@ -44,11 +45,12 @@ bool Shape::collision(square s1, square s2)
     if( abs(s1.m_center.m_x-s2.m_center.m_x)< s1.m_size/2 + s2.m_size/2 + epsil_zero and 
         abs(s1.m_center.m_y-s2.m_center.m_y)< s1.m_size/2 + s2.m_size/2 + epsil_zero)
     {
-        return 1;
+        return true;
     }
-    return 0;
+    return false;
 }
 
+/**METHODES SUR LES POINTS/VECTEURS**/
 double Shape::distance(s_2d point_1, s_2d point_2)
 {
     return pow(pow(point_1.m_x-point_2.m_x,2)+pow(point_1.m_y-point_2.m_x,2), 0.5);
