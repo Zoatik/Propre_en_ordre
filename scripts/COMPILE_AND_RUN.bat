20a6fbc6e15176d06d5f418914f1d285e6a53963
Zoatik <axelhal@hotmail.com>
2023-03-11 01:30:04 +0100

    batch files
    juste parce que ça m'amuse hehe

---
+++ scripts/COMPILE_AND_RUN.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+make
+RUN
+PAUSE
--- scripts/RUN.bat
+++ scripts/RUN.bat
@@ -1,4 +1,11 @@
-@ECHO OFF
-set /p FILE="File name : "
+@echo off
+:while
+set /p FILE=File name : 
 PEO.exe test_files/%FILE%.txt
+set /p CONTINUE=lire un autre fichier ? (O/N) : 
+echo -------------------------------------------
+echo:
+if /i "%CONTINUE%" == "O" (
+	goto :while
+)
 PAUSE
