29be3dc4f765a24c971510970b759a35049264cc
Zoatik <axelhal@hotmail.com>
2023-03-15 23:35:49 +0100

    création de la classe particle
    j'ai rempli le module pour l'utiliser

--- scripts/Particle.cpp
+++ scripts/Particle.cpp
@@ -1,1 +1,16 @@
 #include "Particle.h"
+
+Particle::Particle()
+{
+    //default ctor
+}
+Particle::Particle(s_particle_infos infos)
+{
+    m_square.m_center = infos.m_pos;
+    m_square.m_size = infos.m_d;
+}
+
+square Particle::get_shape()
+{
+    return m_square;
+}
--- scripts/Particle.h
+++ scripts/Particle.h
@@ -1,8 +1,16 @@
+#ifndef PARTICLE_H
+#define PARTICLE_H
 
+#include "File.h"
 
 class Particle
 {
     public:
-    protected:
+        Particle();
+        Particle(s_particle_infos infos);
+        square get_shape();
     private:
+        square m_square;
 };
+
+#endif // PARTICLE_H
